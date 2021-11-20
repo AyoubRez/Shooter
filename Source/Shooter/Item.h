1222608f005661277e0097a0398686f56eb39915
Rezzouk Ayoub <AREZZOUK@pca.ma>
2021-11-20 23:17:24 +0100

    weapon functions widget closes #35 closes #36 closes #37 closes #38 closes #39

---
+++ Source/Shooter/Item.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class SHOOTER_API AItem : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	/** Skeletal Mesh for the item */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Item Properties", meta=(AllowPrivateAccess="true"))
+	class USkeletalMeshComponent* ItemSkeletalMesh;
+
+	/**Line Trace collides with box to show HUD widget*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Item Properties", meta=(AllowPrivateAccess="true"))
+	class UBoxComponent* CollisionBox;
+
+	/** Popup Widget for when the player looks at the item*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Item Properties", meta=(AllowPrivateAccess="true"))
+	class UWidgetComponent* PickUpWidget;
+
+
+public:
+	FORCEINLINE UWidgetComponent* GetPickUpWidget() const { return PickUpWidget; }
+};
