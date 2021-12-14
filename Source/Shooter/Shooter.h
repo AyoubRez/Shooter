646ec5b2532f3fa81d1ef3b85b8355d83d0bcd24
Rezzouk Ayoub <AREZZOUK@pca.ma>
2021-12-14 19:38:51 +0100

    Finish Setup footSteps System closes #219 closes #220 closes #221 closes #222 closes #223 closes #224 closes #225 closes #226 closes #227 closes #228 closes #229 closes #230 closes #231 closes #232

--- Source/Shooter/Shooter.h
+++ Source/Shooter/Shooter.h
@@ -4,3 +4,16 @@
 
 #include "CoreMinimal.h"
 
+#define EPS_Metal EPhysicalSurface::SurfaceType1
+#define EPS_Stone EPhysicalSurface::SurfaceType2
+#define EPS_Tile EPhysicalSurface::SurfaceType3
+#define EPS_Grass EPhysicalSurface::SurfaceType4
+#define EPS_Water EPhysicalSurface::SurfaceType5
+#define EPS_Carpet EPhysicalSurface::SurfaceType6
+#define EPS_Concrete EPhysicalSurface::SurfaceType7
+#define EPS_Gravel EPhysicalSurface::SurfaceType8
+#define EPS_Ground EPhysicalSurface::SurfaceType9
+#define EPS_Plastic EPhysicalSurface::SurfaceType10
+#define EPS_Sand EPhysicalSurface::SurfaceType11
+#define EPS_Snow EPhysicalSurface::SurfaceType12
+#define EPS_Wood EPhysicalSurface::SurfaceType13
